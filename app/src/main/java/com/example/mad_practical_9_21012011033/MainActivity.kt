984a300ad820445d8e1c3035c03d8677be26413b
kj <kkjegoda@gmail.com>
2023-10-12 23:03:32 +0530

    ok

--- app/src/main/java/com/example/mad_practical_9_21012011033/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012011033/MainActivity.kt
@@ -7,21 +7,28 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     lateinit var alarmanimation: AnimationDrawable
+    lateinit var heartanimation: AnimationDrawable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val imageView = findViewById<ImageView>(R.id.imageview)
+        val imageView = findViewById<ImageView>(R.id.alarm)
         imageView.setBackgroundResource(R.drawable.alarm_animation_list)
         alarmanimation = imageView.background as AnimationDrawable
-
+        var heart = findViewById<ImageView>(R.id.heart)
+        heart.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation = heart.background as AnimationDrawable
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
             alarmanimation.start()
+            heartanimation.start()
+
         } else {
             alarmanimation.stop()
+            heartanimation.stop()
+
         }
     }
 }
---
+++ app/src/main/java/com/example/mad_practical_9_21012011033/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mad_practical_9_21012011033
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var logonanimation: AnimationDrawable
+    @SuppressLint("ResourceType", "MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+        val image=findViewById<ImageView>(R.drawable.uvpce_logo)
+        image.setBackgroundResource(R.drawable.uvpce_animation_list)
+        logonanimation=image.background as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if(hasFocus){
+            logonanimation.start()
+        }
+        else{
+            logonanimation.stop()
+        }
+    }
+}
