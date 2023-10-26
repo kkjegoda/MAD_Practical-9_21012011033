bdd335a6b25ebc693ac7298d8d2a89bb85291457
kj <kkjegoda@gmail.com>
2023-10-26 19:48:54 +0530

    practical-9.1

--- app/src/main/java/com/example/mad_practical_9_21012011033/activity_splash.kt
+++ app/src/main/java/com/example/mad_practical_9_21012011033/activity_splash.kt
@@ -1,30 +1,52 @@
 package com.example.mad_practical_9_21012011033
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
-import com.example.mad_practical_9_21012011033.R.drawable.uvpce_logo
 
-class Splash_Activity : AppCompatActivity() {
-    lateinit var logonanimation:AnimationDrawable
-    @SuppressLint("ResourceType")
+class SplashActivity2 : AppCompatActivity(), Animation.AnimationListener {
+    lateinit var gunilogoanimation: AnimationDrawable
+    lateinit var image3:ImageView
+    lateinit var logoanimation:Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash2)
-        val image=findViewById<ImageView>(R.drawable.uvpce_logo)
-        image.setBackgroundResource(R.drawable.uvpce_animation_list)
-        logonanimation=image.background as AnimationDrawable
+
+        image3 = findViewById<ImageView>(R.id.logo)
+        image3.setBackgroundResource(R.drawable.uvpce_animation_list)
+        gunilogoanimation= image3.background as AnimationDrawable
+
+        logoanimation = AnimationUtils.loadAnimation(this , R.anim.twin_animation)
+        logoanimation.setAnimationListener(this)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if(hasFocus){
-            logonanimation.start()
+
+        if (hasFocus){
+            image3.startAnimation(logoanimation)
+            gunilogoanimation.start()
+
         }
         else{
-            logonanimation.stop()
+            gunilogoanimation.stop()
+
         }
+        super.onWindowFocusChanged(hasFocus)
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+        Intent(this , MainActivity::class.java).apply { startActivity(this) }
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
     }
 }
+
